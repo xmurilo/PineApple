9aff01416f18513808383aace2741bfbe183eb6d
Murilo <dixmurilox@gmail.com>
2023-11-22 20:41:43 -0300

    Config: tailwind font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        MainColor: '#642E90',
+      },
+      fontFamily: {
+        SourceSansPro: ['Source Sans Pro', 'sans-serif'],
+        SpliceSans:['Splice Sans', 'sans-serif']
+      },
+      // Faça breakpoints personalizados
+      screens: {
+        xs: '320px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+      },
+    },
   },
   plugins: [],
-}
+};
