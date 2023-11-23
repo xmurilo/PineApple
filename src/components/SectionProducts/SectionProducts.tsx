f79bec3d071869b5eb2bc19cf5ca985140a61b35
Murilo <dixmurilox@gmail.com>
2023-11-23 00:18:06 -0300

    Feat adding path file CardProduct2

--- src/components/SectionProducts/SectionProducts.tsx
+++ src/components/SectionProducts/SectionProducts.tsx
@@ -1,9 +1,11 @@
 import CardProduct1 from './CardProduct1';
+import CardProduct2 from './CardProduct2';
 
 const SectionProducts = () => {
   return (
     <div className='bg-colorBackground w-[90%] m-auto mt-20 rounded-[40px] p-4'>
       <CardProduct1 />
+      <CardProduct2 />
     </div>
   );
 };
