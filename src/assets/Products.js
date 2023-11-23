081f3d0e9378134f0bb30bb0fee0f02eecbbc3c1
Bruno <you@example.com>
2023-11-22 21:56:54 -0300

    feat: creating API from Products

---
+++ src/assets/Products.js
@@ -0,0 +1,51 @@
+export const data = [
+    {
+        id: 1,
+        name: 'PinBook Air de 13 pol.',
+        price: 11.599,
+        description: 'Um notebook leve e potente para fazer de tudo em qualquer lugar.',
+        colors: ['#616161', '#E3E4E5', '#F9D4C2'],
+        photo: 'https://www.apple.com/v/mac/home/bw/images/overview/select/product_tile_mba_13__ci4jh6tvqvyq_large.png'
+    },
+    {
+        id: 2,
+        name: 'iPin Pro',
+        price:  9.799,
+        description: 'A experiência definitiva em iPin com a mais avançada tecnologia.',
+        colors: ['#616161', '#E3E4E5'],
+        photo: 'https://www.apple.com/v/ipad/home/ch/images/overview/compare_ipad_pro__erf9x8mw04sy_large.png'
+        
+
+    },
+    {
+        id: 3,
+        name: 'PinPhone 15 Pro',
+        price: 9.299,
+        description: 'O PinPhone definitivo.',
+        colors: ['#616161', '#E3E4E5', '#F9D4C2', '#434343'],
+        photo: 'https://www.apple.com/v/iphone/home/bp/images/overview/compare/compare_iphone_15_pro__c344odaqs5qq_medium.png'
+    },
+    {
+        id: 4,
+        name: 'PineAPPLE Watch Ultra 2',
+        price: 9.699,
+        description: 'O mais forte e versátil de todos os tempos.',
+        colors: ['#434343'],
+        photo: 'https://www.apple.com/br/watch/images/watch-shared-compare/compare_ultra2__fzfgtok6moeq_medium.jpg'
+    },
+    {
+        id: 5,
+        name: 'PinPods Max',
+        price: 6.590,
+        description: 'O mais forte e versátil de todos, ao seu dispor.',
+        colors: ['#616161', '#E3E4E5', '#F9D4C2'],
+        photo: 'https://www.apple.com/v/airpods/shared/compare/d/images/compare/compare_airpods_max__b14s2x6q07rm_large.png'
+    },
+    {
+        id: 6,
+        name: 'PineAPPLE TV 4K com Wi-Fi',
+        price: 1.499,
+        description: '4K resolution ',
+        photo: 'https://www.apple.com/v/apple-tv-4k/ah/images/overview/compare/apple_4k_wifi__fpjm9mmlrzyy_medium.jpg'
+    },
+]
