b77f0146da64532630f0753e9b69ce22e27f3451
Murilo <dixmurilox@gmail.com>
2023-11-23 00:03:36 -0300

    Feat component SectionProducts

---
+++ src/components/SectionProducts/SectionProducts.tsx
@@ -0,0 +1,11 @@
+import CardProduct1 from './CardProduct1';
+
+const SectionProducts = () => {
+  return (
+    <div className='bg-colorBackground w-[90%] m-auto mt-20 rounded-[40px] p-4'>
+      <CardProduct1 />
+    </div>
+  );
+};
+
+export default SectionProducts;
