b70755aa1d3ea81071fdeb61cff9539fa8c8404a
Murilo <dixmurilox@gmail.com>
2023-11-23 00:01:15 -0300

    Feat adding Typescript to Products data

---
+++ src/assets/Products.ts
@@ -0,0 +1,87 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  details: string[];
+  colors: string[];
+  photo: string;
+}
+
+export const data: Product[] = [
+  {
+    id: 1,
+    name: 'PinBook Air de 13 pol.',
+    price: 11.599,
+    description: 'Um notebook leve e potente para fazer de tudo em qualquer lugar.',
+    details: ['13,3 polegadas', '8 GB de memória', '256 GB de armazenamento', 'Chip M2'],
+    colors: ['#616161', '#E3E4E5', '#F9D4C2'],
+    photo:
+      'https://www.apple.com/v/mac/home/bw/images/overview/select/product_tile_mba_13__ci4jh6tvqvyq_large.png',
+  },
+  {
+    id: 2,
+    name: 'iPin Pro',
+    price: 9.799,
+    description: 'A experiência definitiva em iPin com a mais avançada tecnologia.',
+    details: ['12,9 polegadas', '8 GB de memória', '256 GB de armazenamento', 'Chip M2'],
+    colors: ['#616161', '#E3E4E5'],
+    photo:
+      'https://www.apple.com/v/ipad/home/ch/images/overview/compare_ipad_pro__erf9x8mw04sy_large.png',
+  },
+  {
+    id: 3,
+    name: 'PinPhone 15 Pro',
+    price: 9.299,
+    description: 'O PinPhone definitivo.',
+    details: [
+      '6,7 pol. ou 6,1 pol.',
+      '256 GB de armazenamento',
+      'Fotos em altíssima resolução (24 MP e 48 MP)',
+      'Chip A17',
+    ],
+    colors: ['#616161', '#E3E4E5', '#F9D4C2', '#434343'],
+    photo:
+      'https://www.apple.com/v/iphone/home/bp/images/overview/compare/compare_iphone_15_pro__c344odaqs5qq_medium.png',
+  },
+  {
+    id: 4,
+    name: 'PineAPPLE Watch Ultra 2',
+    price: 9.699,
+    description: 'O mais forte e versátil de todos os tempos.',
+    details: [
+      'Caixa de titânio de 49 mm',
+      'Resistente à água (100m)',
+      '36 horas de bateria',
+      'Chip S9 SiP',
+    ],
+    colors: ['#434343'],
+    photo:
+      'https://www.apple.com/br/watch/images/watch-shared-compare/compare_ultra2__fzfgtok6moeq_medium.jpg',
+  },
+  {
+    id: 5,
+    name: 'PinPods Max',
+    price: 6.59,
+    description: 'O mais forte e versátil de todos, ao seu dispor.',
+    details: [
+      'Áudio espacial personalizado',
+      'Cancelamento de ruído',
+      'Smart Case',
+      '20 horas de bateria',
+    ],
+    colors: ['#616161', '#E3E4E5', '#F9D4C2'],
+    photo:
+      'https://www.apple.com/v/airpods/shared/compare/d/images/compare/compare_airpods_max__b14s2x6q07rm_large.png',
+  },
+  {
+    id: 6,
+    name: 'PineAPPLE TV 4K com Wi-Fi',
+    price: 1.499,
+    description: '4K resolution ',
+    details: ['4K resolução de tela', '64 GB de armazenamento', 'Siro remote', 'Chip A15'],
+    colors: ['#616161', '#E3E4E5', '#F9D4C2'],
+    photo:
+      'https://www.apple.com/v/apple-tv-4k/ah/images/overview/compare/apple_4k_wifi__fpjm9mmlrzyy_medium.jpg',
+  },
+];
