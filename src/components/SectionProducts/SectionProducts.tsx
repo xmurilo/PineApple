f3408011673d93139a514a77e7f68ea527ebc746
Murilo <dixmurilox@gmail.com>
2023-11-25 20:54:27 -0300

    Feat adding Props to SectionProducts

--- src/components/SectionProducts/SectionProducts.tsx
+++ src/components/SectionProducts/SectionProducts.tsx
@@ -1,10 +1,17 @@
+import React from 'react';
 import { data } from '../../assets/Products';
 import CardProduct from './CardProduct';
+import { Product } from '../../assets/interfaceProducts';
 
-const SectionProducts = () => {
+interface SectionMProps {
+  handleAddProduct: (product: Product) => void;
+  products?: Product[];
+}
+
+const SectionProducts: React.FC<SectionMProps> = ({handleAddProduct}) => {
   return (
     <section className='bg-colorBackground w-[90%] m-auto mt-20 rounded-[40px] p-4 flex flex-col items-center '>
-      <CardProduct data={data} />
+      <CardProduct handleAddProduct={handleAddProduct} data={data} />
     </section>
   );
 };
