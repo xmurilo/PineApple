f12d0b16d0c0cd200313f908c82c1ba5ac07cf5e
Murilo <dixmurilox@gmail.com>
2023-11-25 20:58:28 -0300

    Feat  logic of localStorage and Add/RemoveProduct

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import SectionMain from './components/SectionMain';
+import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
+import SectionMain from './components/SectionMain';
 import SectionProducts from './components/SectionProducts/SectionProducts';
+import { Product } from './assets/interfaceProducts';
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [count, setCount] = useState(0);
+
+  const handleAddProduct = (product: Product) => {
+    const products2 = [...products, product];
+    setProducts(products2);
+    setCount(count + 1);
+    localStorage.setItem('products', JSON.stringify(products2));
+  };
+
+  const removeProduct = (index: number) => {
+    const products2 = [...products];
+    products2.splice(index, 1);
+    setProducts(products2);
+    setCount(count - 1);
+    localStorage.setItem('products', JSON.stringify(products2));
+  };
+
+  useEffect(() => {
+    const products = localStorage.getItem('products');
+    if (products) {
+      setProducts(JSON.parse(products));
+      setCount(JSON.parse(products).length);
+    }
+  }, []);
+
   return (
     <>
-      <Navbar />
+      <Navbar count={count} products={products} removeProduct={removeProduct} />
       <SectionMain />
-      <SectionProducts />
+      <SectionProducts handleAddProduct={handleAddProduct} />
     </>
   );
 }
