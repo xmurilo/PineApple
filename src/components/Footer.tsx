8d9c51e87810ce20f1a09c9b4d4d85cf87903495
Bruno <you@example.com>
2023-11-29 16:35:21 -0300

    feat: Footer file

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+
+
+
+const Footer: React.FC = () => {
+
+    return (
+        <footer className='flex items-center justify-center bg-navColor h-16 '>
+        <ul className='flex md:flex-row items-center gap-10 '>
+          <p className="text-yellow-50"><span className="font-semibold">PineAPLE</span> - Todos os direitos reservados @ </p>
+        </ul>
+      </footer>
+    );
+}
+
+export default Footer;
